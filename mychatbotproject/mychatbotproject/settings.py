43b352b69a433148597e1ab5506065e465d3510e
Omotayo Israel <omotayoisrael24@gmail.com>
2025-11-03 11:18:00 +0100

    FINAL FIX

--- mychatbotproject/mychatbotproject/settings.py
+++ mychatbotproject/mychatbotproject/settings.py
@@ -22,7 +22,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure--9=pju1l^7$r5dbe#5^m&ust+p1w6wpw0qjwh9kv7v_5mi8yx$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
